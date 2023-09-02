3583ea940ccefddf622cfbd832ba9d48316b58f7
Andrade Sampaio <sampaio.andrade@outlook.com>
2023-09-02 15:04:52 -0300

    Lambda with python

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def lambda_handler(event, context):
+    print('## ENVIRONMENT VARIABLES')
+    print(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
+    print(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
+    print('## EVENT')
+    print(event)
+    logger.info("Python for the win!")
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
