c752673d0c94859fd53700494422236b4b4133c5
Andrade Sampaio <sampaio.andrade@outlook.com>
2023-09-03 09:43:00 -0300

    pegando tamanho da imagem no s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,31 @@
 import json
 import os
 import logging
+import boto3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
 def lambda_handler(event, context):
-    print('## ENVIRONMENT VARIABLES')
-    print(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
-    print(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
-    print('## EVENT')
-    print(event)
     logger.info("Lambda Python for the win!")
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    path = record['s3']['object']['key']
+    size = get_size(bucket, path)
     return {
         'statusCode': 200,
         'idClient': os.environ['ID_CLIENT'],
+        'size': size,
         'body': json.dumps(event)
     }
+
+def get_size(bucket, path):
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket(bucket)
+    total_size = 0
+
+    for obj in my_bucket.objects.filter(Prefix=path):
+        total_size = total_size + obj.size
+
+    return total_size
