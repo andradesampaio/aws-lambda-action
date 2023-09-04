bf29d982bdb2e4ffcaf3dbfa3edb8b0cca583d69
Andrade Sampaio <sampaio.andrade@outlook.com>
2023-09-04 10:50:40 -0300

    corrigindo a regra de validacao do size s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,17 @@
 import json
 import os
-import logging
 import boto3
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    logger.info("Lambda Python for the win!")
+    print("Lambda Python for the win!")
     record = event['Records'][0]
     bucket = record['s3']['bucket']['name']
     key = record['s3']['object']['key']
     check_size(bucket, key)
+
     return {
         'statusCode': 200,
         'idClient': os.environ['ID_CLIENT'],
@@ -21,12 +20,12 @@ def lambda_handler(event, context):
 
 
 def check_size(bucket, key):
-    s3 = boto3.resource('s3')
-    objeto_s3 = s3.head_object(bucket, key)
-    mega_byte = 1024 * 1024
+    objeto_s3 = s3.head_object(Bucket=bucket, Key=key)
     tamanho_arquivo_bytes = objeto_s3['ContentLength']
 
-    if tamanho_arquivo_bytes > 1 * mega_byte:
-        logger.info("Objeto muito grande")
+    tamanho_arquivo_mb = tamanho_arquivo_bytes / (1024 ** 2)
+
+    if tamanho_arquivo_mb > 1:
+        print("Objeto muito grande")
     else:
-        logger.info("Objeto dentro do tamanho")
+        print("Objeto dentro do tamanho")
